971245a6a471fff7173ae0e6e425794c635bd3f1
julianajlk <julianajlk@gmail.com>
2018-08-08 16:32:10 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,16 @@
 #Placeholder for a model
 
 class Post < ActiveRecord::Base
+  # attr_accessor :name, :content
+  #
+  # POST = []
+  # def initialize
+  #   @name = name
+  #   @content = content
+  #   POST << self
+  # end
+  #
+  # def self.all
+  #   POST
+  # end
 end
